9808bc2228c626d4302a2c4547e8b1e9f5bce225
EyaKadri <eyakaad@gmail.com>
2023-10-15 05:19:04 +0200

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-int main()
+/**
+ * main - Entry point
+ * puts text
+ * Return: Always 0
+ */
+int main(void)
 {
-    
-    puts("Programming is like building a multilingual puzzle");
- 
-    return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
