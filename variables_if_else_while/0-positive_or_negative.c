032f1ada016ba9242f67b28e6c5ddb4af5d7105b
EyaKadri <eyakaad@gmail.com>
2023-10-16 13:24:29 +0200

    0-positive_or_negative

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -13,15 +13,14 @@
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf("int is ", n);
-
+	printf(" %d is ");
 	if (n > 0)
 	{
-		printf("int is positive");
+		printf("positive");
 	}
 	else if (n < 0)
 	{
-		printf("int is negative");
+		printf("negative");
 	}
 	else
 	{
