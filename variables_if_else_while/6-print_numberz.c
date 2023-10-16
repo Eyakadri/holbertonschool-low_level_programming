85b0148df31943ab83c817c9b4bb739c05115f9a
EyaKadri <eyakaad@gmail.com>
2023-10-16 15:12:33 +0200

    6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+*main - main function
+*
+*Return: 0
+*/
+int main(void)
+{
+int x = 0;
+
+while (x < 10)
+{
+putchar(x + '0');
+if (x < 9)
+{
+}
+x++;
+}
+putchar('\n');
+
+return (0);
+}
