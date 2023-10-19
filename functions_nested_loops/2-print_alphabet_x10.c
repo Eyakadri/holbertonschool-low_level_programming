21219ff28ffaec113b2fd0ddf7dfbe8140e23143
EyaKadri <eyakaad@gmail.com>
2023-10-19 12:10:07 +0200

    2-alphabet_x10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet - alphabet a-z 10 times
+ *
+ * Return: Void
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
+}
