14b97f7824239d1effa5e7d4da371e963406122e
EyaKadri <eyakaad@gmail.com>
2023-11-02 16:01:51 +0100

    5-string_toupper.c

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * *string_toupper - a function that changes all lowercase letters
+ *@a: char
+ * Return: str
+ */
+char *string_toupper(char *a)
+{
+	int i = 0;
+
+	if (*a != '\0')
+	{
+		while (*(a + i) != '\0')
+		{
+			if (*(a + i) <= 'z' && *(a + i) >= 'a')
+				*(a + i) -= 32;
+			i++;
+		}
+	}
+	return (s);
+}
