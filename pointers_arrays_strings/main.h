c1369c69cadae85d8699fea73250bd4c81d68f94
EyaKadri <eyakaad@gmail.com>
2023-11-02 13:46:19 +0100

    1-strncat

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 int _putchar(char c);
+char *_strncat(char *dest, char *src, int n);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -13,4 +14,3 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-char *_strncat(char *dest, char *src, int n);
