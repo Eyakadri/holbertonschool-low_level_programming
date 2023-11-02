565bc9de4add2a1f90d9045d304f180d35606665
EyaKadri <eyakaad@gmail.com>
2023-11-02 16:22:08 +0100

    7-leet.c

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -4,26 +4,25 @@
  * @str: char
  * Return: str
  */
-char *leet(char *str)
+char *leet(char *s)
 {
 	int a, b;
-
 	char c[] = "aAeEoOtTlL";
 	char d[] = "4433007711";
 
 	a = 0;
-	while (str[a] != '\0')
+	while (s[a] != '\0')
 	{
 		b = 0;
 		while (c[b] != '\0')
 		{
-			if (str[a] == c[b])
+			if (s[a] == c[b])
 			{
-				str[a] = n[b];
+				s[a] = d[b];
 			}
 			b++;
 		}
 		a++;
 	}
-	return (str);
+	return (s);
 }
