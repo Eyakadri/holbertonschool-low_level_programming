c76a5f695696cb16fcfc5b2b3c4375c09b604a21
EyaKadri <eyakaad@gmail.com>
2023-11-02 14:54:29 +0100

    2-strncpy.c

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a;
+	a = 0;
+	while (a < n && src[a] != '\0')
+	{
+		dest[a] = src[a];
+		a++;
+	}
+	a = a;
+	while (a < n)
+	{
+		dest[a] = '\0';
+		a++;
+	}
+	return (dest);
+}
