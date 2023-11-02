94ebbb3dca13b40747841685e9a84b0fc6dfc109
EyaKadri <eyakaad@gmail.com>
2023-11-02 14:58:16 +0100

    2-strncpy.c

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,8 +1,15 @@
 #include "main.h"
-
+/**
+ * _strncpy - a function that copies a string
+ * @dest: char
+ * @src: char
+ * @n: int
+ * Return: dest
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int a;
+
 	a = 0;
 	while (a < n && src[a] != '\0')
 	{
