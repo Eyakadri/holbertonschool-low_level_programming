180ceb3e0522b9070e002c258b3ee1c72d692a2d
EyaKadri <eyakaad@gmail.com>
2023-11-03 13:19:54 +0100

    4-strpbrk.c

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strpbrk -  a function that searches a string
+ * @s: string
+ * @accept: char
+ * Return: a or null
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	{
+		char *a, *b;
+
+		for (a = s; *a != '\0'; a++)
+		{
+			for (b = accept; *b != '\0'; b++)
+			{
+				if (*a == *b)
+				{
+					return (a);
+				}
+			}
+		}
+
+		return (NULL);
+	}
+}
