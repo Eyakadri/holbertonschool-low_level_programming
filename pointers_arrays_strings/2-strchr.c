e8e39b5107f5a6b4c2bb784126ccdf209e04b224
EyaKadri <eyakaad@gmail.com>
2023-11-03 11:59:24 +0100

    2-strchr.c

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -4,12 +4,12 @@
  * @s: string
  * @c: character
  * Return: s or NULL
- */
+*/
 char *_strchr(char *s, char c)
 {
-	if (*s == c)
+	while (*s != '\0')
 	{
-		while (*s != '\0')
+		if (*s == c)
 		{
 			return (s);
 		}
