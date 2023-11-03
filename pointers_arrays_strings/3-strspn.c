89db339ba93bb1dd7514ec4f36c74880382f3a52
EyaKadri <eyakaad@gmail.com>
2023-11-03 12:42:41 +0100

    3-strspn.c

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,6 +1,20 @@
 #include "main.h"
-
+/**
+ * _strspn -  a function that gets the length of a prefix substring
+ * @s: substring
+ * @accept: char
+ * Return: b
+ */
 unsigned int _strspn(char *s, char *accept)
 {
-    
+	{
+		int a, b = 0;
+
+		while (s1[a] != '\0' && b == 0)
+		{
+			b = s1[a] - s2[a];
+			a++;
+		}
+		return (b);
+	}
 }
