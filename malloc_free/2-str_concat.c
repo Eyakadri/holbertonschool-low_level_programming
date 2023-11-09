5f400f50832ef3bac641f02cc048916265c4e34f
EyaKadri <eyakaad@gmail.com>
2023-11-09 12:49:14 +0100

    2-str_concat.c

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+char *str_concat(char *s1, char *s2)
+{
+	int len1, len2;
+	char *c;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+	c = malloc(len1 + len2 + 1);
+	strcpy(c, s1);
+	strcat(c, s1);
+	return (c);
+}
