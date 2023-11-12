bd9dd72ed2d734b95f27831834d04da8cf8469d2
EyaKadri <eyakaad@gmail.com>
2023-11-12 21:43:37 +0100

    1-string_nconcat.c

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -4,7 +4,7 @@
  * @s1: first string
  * @s2: second string
  * @n: number of bytes of s2 to concatenate to s1
- * Return: pointer to new string
+ * Return: ptr
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
