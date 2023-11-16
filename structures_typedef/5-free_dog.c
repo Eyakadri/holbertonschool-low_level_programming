bfe19e69cf494de2268053530a584dae36355272
EyaKadri <eyakaad@gmail.com>
2023-11-16 11:52:28 +0100

    5-free_dog.c

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog -  a function that frees dogs
+ * @d: pointer to free dogs
+*/
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+	}
+}
