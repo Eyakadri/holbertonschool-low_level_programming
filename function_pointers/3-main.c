6d8bc5f30d8cc58ca92e09c608d970c37b620739
EyaKadri <eyakaad@gmail.com>
2023-11-20 14:21:16 +0100

    3-op_functions.c

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -8,23 +8,23 @@
  */
 int main(int argc, char *argv[])
 {
-	int a, b, c;
-	int (*f)(int, int);
+    int a, b, c;
+    int (*f)(int, int);
 
-	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	 c= get_op_func(argv[2]);
-	if (c == NULL)
-	{
-		printf("Error\n");
-		exit(99);
-	}
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-	
-	printf("%d\n", f(a, b));
-	return (0);
+    if (argc != 4)
+    {
+        printf("Error\n");
+        exit(98);
+    }
+    f = get_op_func(argv[2]);
+    if (f == NULL)
+    {
+        printf("Error\n");
+        exit(99);
+    }
+    a = atoi(argv[1]);
+    b = atoi(argv[3]);
+    c = f(a, b);
+    printf("%d\n", c);
+    return (0);
 }
