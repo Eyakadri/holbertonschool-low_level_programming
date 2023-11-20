30d7d9c0a428c68528c2aa9e02a79b4c5ae4b2d3
EyaKadri <eyakaad@gmail.com>
2023-11-20 20:55:19 +0100

    0-sum_them_all.c

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,3 +1,5 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 #include <stdio.h>
 #include <stdarg.h>
 int _putchar(char c);
@@ -5,3 +7,4 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+#endif
