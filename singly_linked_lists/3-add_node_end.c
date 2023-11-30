9c116c18c6515d2c54148024aa1b20fd2e9bb084
EyaKadri <eyakaad@gmail.com>
2023-11-30 10:00:34 +0100

    3-add_node_end.c

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,31 +1,29 @@
 #include "lists.h"
 /**
  * add_node_end - a function that adds a new node at the end of a list
- * @head: .
- * @str: .
- *Return: new node
-*/
+ * @head: pointer to the head of the list
+ * @str: string to be added to the list
+ * Return: pointer to the new node
+ */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node;
 	list_t *node_end;
-	int i;
 
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
-		return (NULL);
-	new_node->str = strdup(str);
-	for (i = 0; str[i] != '\0'; i++)
-	new_node->len = i;
-	new_node->next = NULL;
+	node_end = malloc(sizeof(list_t));
+	node_end->str = strdup(str);
+	node_end->len = strlen(str);
+	node_end->next = NULL;
 	if (*head == NULL)
-		*head = new_node;
+		*head = node_end;
 	else
 	{
-		node_end = *head;
-		while (node_end->next != NULL)
-			node_end = last_node->next;
-		node_end->next = new_node;
+		list_t *last_node = *head;
+
+		while (last_node->next != NULL)
+		{
+			last_node = last_node->next;
+		}
+		last_node->next = node_end;
 	}
-	return (new_node);
+	return (node_end);
 }
