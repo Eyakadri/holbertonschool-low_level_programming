572d4134241dbc7400a48bf88badfabe39d756b1
EyaKadri <eyakaad@gmail.com>
2023-11-30 10:13:59 +0100

    3-add_node_end.c

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -10,20 +10,23 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *node_end;
 
 	node_end = malloc(sizeof(list_t));
+
 	if (node_end == NULL)
 	{
 		free(node_end);
-		return(NULL);
+		return (NULL);
 	}
-	
+
 	node_end->str = strdup(str);
+
 	if (node_end->str == NULL)
 	{
 		free(node_end);
-		return(NULL);
+		return (NULL);
 	}
 	node_end->len = strlen(str);
 	node_end->next = NULL;
+
 	if (*head == NULL)
 		*head = node_end;
 	else
